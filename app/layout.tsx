e18213514a7374b86c551f9aa95d00253312adeb
Reejay2003 <156231735+Reejay2003@users.noreply.github.com>
2025-03-13 19:45:52 +0545

    Hydration Resolved

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,9 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
+      <body suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      ><span suppressHydrationWarning>{children}</span>
+      >{children}
       </body>
     </html>
   );
